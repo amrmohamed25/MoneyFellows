9986a0631c54e61a0ffef6f2c2f867600fee6740
Amr Mohamed Awad <amrmohamedawed80@gmail.com>
2021-08-27 20:48:22 +0200

    update

---
+++ storage/framework/views/4a81148c2f75b2d9127c74d91339032ab9d27773.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH F:\Training\RMZ\moneyfellows.github.io\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8edb4861d38cecfc66093c8c40f769b2b3053e28.php
@@ -0,0 +1,2 @@
+[<?php echo e($slot); ?>](<?php echo e($url); ?>)
+<?php /**PATH F:\Training\RMZ\moneyfellows.github.io\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
